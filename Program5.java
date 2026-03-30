8dc3cdffae83560237ae27d6a00f90ee65429581
RayZ24680 <120994695+RayZ24680@users.noreply.github.com>
2026-03-30 17:24:38 -0500

    Implement ACCESSTYPE enum with access methods
    The use of java enums and their singleton property helps distinguish the access types and their method override property helps deal with them accordingly. Without the enums each access type will have to be a subclass of its own.

---
+++ Program5.java
@@ -0,0 +1,66 @@
+public class Pragram5{
+  public static void main(String[] args){
+    ACCESSTYPE a = ACCESSTYPE.READ;
+    ACCESSTYPE b = ACCESSTYPE.READ_WRITE;
+    ACCESSTYPE c = ACCESSTYPE.FULL_ACCESS;
+
+    a.read();//Allowed
+    a.write();//denied
+    b.write();//allowed
+    b.read();//allowed
+    c.read();//allowed
+    a.execute();//denied
+    b.execute();//denied
+    c.execute();//allowed
+  }
+
+  public enum ACCESSTYPE{
+    READ{
+      public void read(){
+        print("Read is allowed.");
+      }
+    },
+    WRITE{
+      public void write(){
+        print("Writing allowed.");
+      }
+    },
+    EXECUTE{
+      public void execute(){
+        print("Executing is allowed.");
+      }
+    },
+    READ_WRITE{
+      public void write(){
+        print("Writing is allowed.");
+      }
+      public void read(){
+        print("Reading is allowed.");
+      }
+    },
+    FULL_ACCESS{
+      public void read(){
+        print("Reading is allowed.");
+      }
+      public void write(){
+        print("Writing is allowed.");
+      }
+      public void execute(){
+        print("Executing is allowed.");
+      }
+    };
+    public void read(){
+      print("ERRO: Read not allowed!");
+    }
+    public void write(){
+      print("ERRO: writing not allowed!");
+    }
+    public void execute(){
+      print("ERRO: Execute not allowed!");
+    }
+  }
+  public static void print(String s){
+    System.out.println(s);
+  }
+}
+
