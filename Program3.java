5cb14bd564fec088f92397214fb81eb36ebc3054
RayZ24680 <120994695+RayZ24680@users.noreply.github.com>
2026-03-30 21:23:16 -0500

    Refactor SHAPE enum and update area method calls
    Refactor SHAPE enum to include area methods without parameters and adjust constructors. Update area method calls in main method.

--- Program3.java
+++ Program3.java
@@ -2,24 +2,44 @@ public class Program3{
 
 
   public enum SHAPE{
-    CIRCLE{
+    CIRCLE(2.0){
       @Override
       public double area(double r){
         return Math.PI * r * r;
       }
+      public double area(){
+          return Math.PI * this.r * this.r;
+      }
     },
-    RECTANGLE{
+    RECTANGLE(4.0,5.0){
       public double area(double l, double h){
         return l*h;
       }
+      public double area(){
+          return this.len * this.wid;
+      }
     },
-    TRIANGLE{
+    TRIANGLE(4.0,2.0){
       public double area(double l, double h){
         return (0.5) * l * h;
       }
+      public double area(){
+          return 0.5 * this.wid * this.len;
+      }
     };
-    public abstract double area(double l, double h);
-    public abstract double area(double r);
+    public double r;
+    public double len;
+    public double wid;
+    SHAPE(double r){
+        this.r = r;
+    }
+    SHAPE(double l, double w){
+        this.wid = w;
+        this.len = l;
+    }
+    public double area(double l, double h){return 0.0;}
+    public double area(double r){return 0.0;}
+    public double area(){return 0.0;}
     //Each enum instance can overrride the commonly desclared methods in the enum namespace.
   }
 
@@ -31,9 +51,10 @@ public class Program3{
     SHAPE circle = null;
     SHAPE tri = SHAPE.TRIANGLE;
 
-    print(rect.area(3.0,4.0));
-    print(tri.area(3.0,4.0));
-    // print(circle.area(2.0)); causes NULLPOINTEREXCEPTION.
+    print(rect.area(3.0,4.0));//works
+    print(rect.area());//works
+    print(tri.area(3.0,4.0));//works
+    // print(circle.area(2.0));// error cicle may not have been initialized (error);
     //using unassigned variables or null assigned variables can lead to not initialized error or NULLPOINTEREXCEPTION.
     
   }
